0490299e4a51a84b66268bdc9b493d233f5c78f0
GuilhermeQuirinoCruz <guilhermequirinocruz@gmail.com>
2024-05-25 18:52:42 -0300

    processar strings com afd

--- src/afd/afd.py
+++ src/afd/afd.py
@@ -8,10 +8,43 @@ class AFD:
     def __init__(self, alphabet) -> None:
         self.alphabet = alphabet
         self.states = {}
+        self.start_state = ''
     
     def add_state(self, name, type='none'):
         new_state = AFDState(name, type)
         self.states.update({name : new_state})
 
+        if type == 'start':
+            self.start_state = name
+
     def add_transition_to_state(self, state, symbol, next_state):
         self.states.get(state).transitions.update({symbol : next_state})
+
+    def proccess_string(self, string, print_info=False):
+        if print_info:
+            print(f'Processing string {string}')
+        
+        current_state = self.start_state
+
+        for symbol in string:
+            if print_info:
+                print(f'Current state: {current_state}')
+                print(f'Reading symbol: {symbol}')
+            
+            next_state = self.states.get(current_state).transitions.get(symbol)
+            if not next_state:
+                if print_info:
+                    print('Transition not found')
+                
+                return False
+            
+            if print_info:
+                print(f'Transitioning to state: {next_state}')
+
+            current_state = next_state
+
+        accept_string = self.states.get(current_state).type == 'accept'
+        if print_info:
+            print('String accepted' if accept_string else 'String rejected')
+        
+        return accept_string
--- src/main.py
+++ src/main.py
@@ -2,8 +2,6 @@ import afd.afd as AFD
 import afn
 import afn_e
 
-print('Projeto de Teoria da Computação')
-
 new_afd = AFD.AFD(['a', 'b'])
 new_afd.add_state('q0', 'start')
 new_afd.add_transition_to_state('q0', 'a', 'q1')
@@ -17,8 +15,9 @@ new_afd.add_state('q2')
 new_afd.add_transition_to_state('q2', 'a', 'q1')
 new_afd.add_transition_to_state('q2', 'b', 'qf')
 
-new_afd.add_state('qf', 'final')
+new_afd.add_state('qf', 'accept')
 new_afd.add_transition_to_state('qf', 'a', 'qf')
 new_afd.add_transition_to_state('qf', 'b', 'qf')
 
-print(new_afd.states.get('q0').transitions)
+new_afd.proccess_string('aabbaa', print_info=True)
+new_afd.proccess_string('ababa', print_info=True)
