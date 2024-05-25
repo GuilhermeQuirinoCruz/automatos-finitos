da6f7aa8b85353e03a326e70321a29572bdebb74
GuilhermeQuirinoCruz <guilhermequirinocruz@gmail.com>
2024-05-25 20:12:45 -0300

    leitura de arquivos, formato ajustado pra usar listas

--- src/main.py
+++ src/main.py
@@ -1,23 +1,33 @@
+import parser.yaml_parser as YAMLParser
+
 import afd.afd as AFD
 import afn
 import afn_e
 
-new_afd = AFD.AFD(['a', 'b'])
-new_afd.add_state('q0', 'start')
-new_afd.add_transition_to_state('q0', 'a', 'q1')
-new_afd.add_transition_to_state('q0', 'b', 'q2')
+def test_afd():
+    new_afd = AFD.AFD(['a', 'b'])
+    new_afd.add_state('q0', 'start')
+    new_afd.add_transition_to_state('q0', 'a', 'q1')
+    new_afd.add_transition_to_state('q0', 'b', 'q2')
 
-new_afd.add_state('q1')
-new_afd.add_transition_to_state('q1', 'a', 'qf')
-new_afd.add_transition_to_state('q1', 'b', 'q2')
+    new_afd.add_state('q1')
+    new_afd.add_transition_to_state('q1', 'a', 'qf')
+    new_afd.add_transition_to_state('q1', 'b', 'q2')
 
-new_afd.add_state('q2')
-new_afd.add_transition_to_state('q2', 'a', 'q1')
-new_afd.add_transition_to_state('q2', 'b', 'qf')
+    new_afd.add_state('q2')
+    new_afd.add_transition_to_state('q2', 'a', 'q1')
+    new_afd.add_transition_to_state('q2', 'b', 'qf')
 
-new_afd.add_state('qf', 'accept')
-new_afd.add_transition_to_state('qf', 'a', 'qf')
-new_afd.add_transition_to_state('qf', 'b', 'qf')
+    new_afd.add_state('qf', 'accept')
+    new_afd.add_transition_to_state('qf', 'a', 'qf')
+    new_afd.add_transition_to_state('qf', 'b', 'qf')
 
-new_afd.proccess_string('aabbaa', print_info=True)
-new_afd.proccess_string('ababa', print_info=True)
+    new_afd.proccess_string('aabbaa', print_info=True)
+    new_afd.proccess_string('ababa', print_info=True)
+
+def test_yaml_parser_afd():
+    parser = YAMLParser.YAMLParser()
+    # afd = parser.parse_afd('test.yaml')
+    afd = parser.parse_afd('../../tests/examples/ex1.yaml')
+
+test_yaml_parser_afd()
---
+++ src/parser/yaml_parser.py
@@ -0,0 +1,35 @@
+import yaml
+
+import os
+from collections import Counter
+
+import afd.afd as AFD
+
+# https://gist.github.com/pypt/94d747fe5180851196eb
+# https://stackoverflow.com/questions/44904290/getting-duplicate-keys-in-yaml-using-python
+class PreserveDuplicatesLoader(yaml.loader.Loader):
+    def construct_mapping(loader, node, deep=False):
+        keys = [loader.construct_object(node, deep=deep) for node, _ in node.value]
+        vals = [loader.construct_object(node, deep=deep) for _, node in node.value]
+        key_count = Counter(keys)
+        data = {}
+        for key, val in zip(keys, vals):
+            if key_count[key] > 1:
+                if key not in data:
+                    data[key] = []
+                data[key].append(val)
+            else:
+                data[key] = val
+        return data
+
+class YAMLParser():
+    def parse_afd(self, path):
+        with open(os.path.join(os.path.dirname(__file__), path), 'r') as file:
+            # afd_data = yaml.load(file, Loader=PreserveDuplicatesLoader)
+            afd_data = yaml.safe_load(file)
+        
+        afd = AFD.AFD(afd_data['alphabet'])
+        for state in afd_data['states']:
+            print(state)
+
+        return afd
