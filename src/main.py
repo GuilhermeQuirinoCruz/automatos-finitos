5776a5f979d5784b4573a28bf30dae190d1221aa
GuilhermeQuirinoCruz <guilhermequirinocruz@gmail.com>
2024-05-25 20:16:53 -0300

    types alterado pra tupla

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import afn_e
 
 def test_afd():
     new_afd = AFD.AFD(['a', 'b'])
-    new_afd.add_state('q0', 'start')
+    new_afd.add_state('q0', ('start',))
     new_afd.add_transition_to_state('q0', 'a', 'q1')
     new_afd.add_transition_to_state('q0', 'b', 'q2')
 
@@ -18,7 +18,7 @@ def test_afd():
     new_afd.add_transition_to_state('q2', 'a', 'q1')
     new_afd.add_transition_to_state('q2', 'b', 'qf')
 
-    new_afd.add_state('qf', 'accept')
+    new_afd.add_state('qf', ('accept',))
     new_afd.add_transition_to_state('qf', 'a', 'qf')
     new_afd.add_transition_to_state('qf', 'b', 'qf')
 
